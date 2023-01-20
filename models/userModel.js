98d04596b376e14005f18bafc68ea1c2fef72886
Prashant <prashantlakhera260394@gmail.com>
2023-01-20 14:25:25 +0530

    unique identifier added

--- models/userModel.js
+++ models/userModel.js
@@ -4,10 +4,12 @@ const userSchema = new mongoose.Schema({
         type:String
     },
     email:{
-        type:String
+        type:String,
+        unique:true
     }, 
     phoneNumber:{
-        type:Number
+        type:Number,
+        unique:true
     }
     
   });
